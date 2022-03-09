671ab190fedf75d446e2d333549f65aa3bccafe1
Nick Camillo <nick.camillo@gmail.com>
2022-03-09 10:58:37 -0500

    add reject unauthorized flag and dimensions

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
-import { Editframe, CommonResolutions } from "@editframe/editframe-js";
+import { Editframe } from "@editframe/editframe-js";
 import dotenv from "dotenv";
 
 dotenv.config();
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
 const main = async () => {
   const editframe = new Editframe({
     clientId: process.env.EDITFRAME_CLIENT_ID,
+    host: "https://api.editframe.test",
     token: process.env.EDITFRAME_API_TOKEN,
   });
 
   const composition = await editframe.videos.new({
-    dimensions: CommonResolutions._1080pVertical,
+    dimensions: {
+      height: 1080,
+      width: 1920,
+    },
     duration: 10,
   });
 
